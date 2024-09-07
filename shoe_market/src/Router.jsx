2cf1ab516b661f67067421e8f57e79a053a0e58d
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2024-09-08 01:20:28 +0200

    Ajout du header

--- shoe_market/src/Router.jsx
+++ shoe_market/src/Router.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 // Import des pages
 import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
+
+// Import du composant ProtectedRoute
+import ProtectedRoute from "./components/ProtectedRoute";
+import HomePage from "./pages/HomePage";
 
 // Définir les routes publiques
 const publicRoutes = [
@@ -11,13 +16,29 @@ const publicRoutes = [
   { path: "/signup", element: <Signup /> },
 ];
 
+// Définir les routes protégées
+const protectedRoutes = [{ path: "/home", element: HomePage }];
+
 const Router = () => {
+  const [user, setUser] = useState({});
+
   return (
     <BrowserRouter>
       <Routes>
         {/* Routes publiques */}
         {publicRoutes.map(({ path, element }, index) => (
           <Route key={index} path={path} element={element} />
+        ))}
+
+        {/* Routes protégées */}
+        {protectedRoutes.map(({ path, element: Element }, index) => (
+          <Route
+            key={index}
+            path={path}
+            element={
+              <ProtectedRoute element={Element} user={user} setUser={setUser} />
+            }
+          />
         ))}
       </Routes>
     </BrowserRouter>
---
+++ shoe_market/src/components/ProtectedRoute.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const ProtectedRoute = ({ element: Component, user, setUser }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const tokenCheck = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        navigate("/login");
+        return;
+      }
+
+      try {
+        const response = await axios.get(
+          "http://localhost:4100/user/token-check",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        // Stocker les informations sur l'utilisateur
+        setUser(response.data.user);
+        console.log(response.data);
+        // Si le token expire dans moins de 5 minutes, stocker un nouveau token
+        if (response.data.newToken) {
+          localStorage.setItem("token", response.data.newToken);
+        }
+      } catch (error) {
+        console.error("Error fetching protected resource:", error);
+        navigate("/login");
+      }
+    };
+
+    tokenCheck();
+  }, [navigate, setUser]);
+
+  return <Component user={user} />;
+};
+
+export default ProtectedRoute;
